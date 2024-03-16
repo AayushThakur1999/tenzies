d32cb471a0e6f014abd5a78291e3766e0d441061
Aayush Thakur <ashcold21@gmail.com>
2024-03-16 18:22:42 +0530

    add roll count

--- src/App.jsx
+++ src/App.jsx
@@ -103,7 +103,6 @@ function App() {
         <div>
           {`Rolls:${rollCount}`}
         </div>
-        
       </div>
       <h1 className='title'>Tenzies</h1>
       <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
